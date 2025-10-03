07cbff80b3ab5bf1f8ee639170aa9e8db791b50d
izzalDev <rizal.fadlullah@gmail.com>
2025-10-03 17:39:22 +0700

    Added related entity: Image, Kernel, and GaussianKernel

---
+++ src/pcd_task_1/gaussian_kernel.py
@@ -0,0 +1,4 @@
+from kernel import Kernel 
+
+class GaussianKernel(Kernel):
+    pass
---
+++ src/pcd_task_1/image.py
@@ -0,0 +1,10 @@
+from pcd_task_1.kernel import Kernel
+
+
+class Image:
+    def convolve2D(self, kernel: Kernel)->Image:
+        pass
+
+    def corelate2D(self, kernel: Kernel)->Image:
+        pass
+
